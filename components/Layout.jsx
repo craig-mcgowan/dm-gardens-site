035194ab945266045b6341a380a2c6390bdaf68d
Craig McGowan <craigmcgowan15@gmail.com>
2022-09-12 21:04:26 -0400

    test imports

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,5 +1,5 @@
-import Navbar from "/components/Navbar.jsx";
-import Footer from "/components/Footer.jsx";
+import Navbar from "./Navbar.jsx";
+import Footer from "./Footer.jsx";
 import styles from "../styles/Layout.module.css";
 
 export default function Layout({ children }) {
