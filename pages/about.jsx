aad5ffea09f263c605327c44b9a80676588949c5
Craig McGowan <craigmcgowan15@gmail.com>
2022-09-13 20:40:09 -0400

    add about Dan blurb

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,8 +1,17 @@
 export default function About() {
   return (
-    <img
-      src="https://res.cloudinary.com/dm9ig1vtk/image/upload/v1663032945/Dan%20Miller%20Gardens/DM_Headshot_myo4n6.webp"
-      alt="Dan Miller"
-    />
+    <>
+      <h1>About Dan</h1>
+      <img
+        src="https://res.cloudinary.com/dm9ig1vtk/image/upload/v1663032945/Dan%20Miller%20Gardens/DM_Headshot_myo4n6.webp"
+        alt="Dan Miller"
+      />
+      <p>
+        Dan is a landscape and garden professional with over 10 years experience.
+        Landscape services including planting, hardscaping, irrigation and
+        lighting. My goal is to improve the functionality and beauty of your
+        outdoor space while providing exceptional service.
+      </p>
+    </>
   );
 }
