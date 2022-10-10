5c75aa053688ac9e0b110e7d6c629ecd0848a9af
Craig McGowan <craigmcgowan15@gmail.com>
2022-10-10 16:06:24 -0400

    form styling

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,32 +1,100 @@
+import { useEffect, useState } from "react"
+import {useForm} from "react-hook-form"
 import styles from "../styles/Contact.module.css"
 
+
 export default function Contact() {
+  const emptyForm = {
+    name: "",
+    email: "",
+    phone: "",
+    contactPref: "email",
+    subject: "",
+    body: "",
+  }
+  
+  const {
+    register,
+    reset,
+    handleSubmit,
+    watch,
+    formState: { isSubmitting, isSubmitSuccessful, isSubmitted },
+  } = useForm({ defaultValues: emptyForm })
+;
+
+  
+  // const [formData, setFormData] = useState(emptyForm)
+  const onSubmit = async (data) => {
+
+    const res = await fetch('/api/form', {
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method:'POST'
+    })
+    const result = await res.json()
+    console.log(result)
+  }
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset();
+  }}, [isSubmitSuccessful, reset]);
+
+
   return (
     <>
       <h2>Request a Quote</h2>
-      <form action="" method="get" className={styles.form}>
-        <fieldset className={styles.fieldset}> 
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        method="post"
+        className={styles.form}
+      >
+        <fieldset className={styles.fieldset}>
           {/* <legend>Contact Dan Miller Gardens</legend> */}
+          <label htmlFor="name">Your Name: </label>
+          <input
+            type="text"
+            autoComplete="name"
+            {...register("name", { required: true })}
+          />
           <label htmlFor="email">Your Email: </label>
-          <input type="email" name="email" id="email" />
+          <input
+            type="email"
+            autoComplete="email"
+            {...register("email", { required: true })}
+          />
           <label htmlFor="phone">Your Phone Number:</label>
-          <input type="tel" name="phone" id="phone" />
+          <input
+            type="tel"
+            autoComplete="phone"
+            {...register("phone", { required: false })}
+          />
           <p>Preferred contact method:</p>
           <div>
-            <label htmlFor="radioEmail">Email</label>  
-            <input type="radio" name="contactPref" id="radioEmail" value="email" />
-            <label htmlFor="radioCall">Phone Call</label>  
-            <input type="radio" name="contactPref" id="radioCall" value="call"/>
-            <label htmlFor="radioText">Text Message</label>  
-            <input type="radio" name="contactPref" id="radioText" value="text"/> 
+            <label htmlFor="radioEmail">Email</label>
+            <input type="radio" {...register("contactPref")} value="email" />
+            <label htmlFor="radioCall">Phone Call</label>
+            <input type="radio" {...register("contactPref")} value="call" />
+            <label htmlFor="radioText">Text Message</label>
+            <input type="radio" {...register("contactPref")} value="text" />
           </div>
           <label htmlFor="subject">Subject:</label>
-          <input type="text" name="subject" id="subject" />
-          <label htmlFor="body">Message:</label>
-          <textarea name="body" id="body" className={styles.textarea} />
-          <input type="submit" value="Submit" />
+          <input
+            type="text"
+            autoComplete="subject"
+            {...register("subject", { required: true })}
+          />
+          <label htmlFor="message">Message:</label>
+          <textarea
+            className={styles.textarea}
+            {...register("message", { required: true })}
+          />
+          <button disabled={isSubmitting} className={isSubmitSuccessful? styles.submitted : styles.submit}>
+            {isSubmitting ? "Saving" : "Submit"}
+          </button>
         </fieldset>
       </form>
     </>
-  )
+  );
 }
