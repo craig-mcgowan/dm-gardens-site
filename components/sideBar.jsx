aa6c96067b8c7cdb5e25da335b2ce0ebb7e7642c
Craig McGowan <craigmcgowan15@gmail.com>
2022-10-15 11:24:40 -0400

    initial sidebar component set up

---
+++ components/sideBar.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react"
+import NavLinks from "./navLinks"
+
+const SideBar = (props) => {
+  return (
+    <div className={}>
+      <NavLinks/>
+    </div>
+  )
+
+}
