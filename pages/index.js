b3a620b2552aa654abbd237af53a80883453b26c
Craig McGowan <craigmcgowan15@gmail.com>
2022-10-17 22:05:50 -0400

    styling index page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import styles from '../styles/Index.module.css'
-
+import services from '../public/services';
 
 export default function Home() {
   return (
@@ -12,6 +12,16 @@ export default function Home() {
           (585)-737-3143 or email danmillergardens@gmail.com.
         </p>
       </div>
+      <section className={styles.services}>
+        {services.map((service) => {
+          return (
+            <figure className={styles.serviceCard}>
+              <img className={styles.serviceThumb } src={service.img}></img>
+              <figcaption>{service.text}</figcaption>
+           </figure>
+         )
+       })}
+      </section>
     </div>
   );
 }
---
+++ public/services.js
@@ -0,0 +1,28 @@
+const services = [
+  {
+    text: "Garden Design",
+    img: "https://res.cloudinary.com/dm9ig1vtk/image/upload/v1663018679/Dan%20Miller%20Gardens/image_3_ogitz3.jpg",
+  },
+  {
+    text: "Plantings",
+    description: "",
+    img: "https://res.cloudinary.com/dm9ig1vtk/image/upload/v1663018679/Dan%20Miller%20Gardens/image_1_uo8tnt.jpg",
+  },
+  {
+    text: "Maintenance",
+    description: "",
+    img: "https://res.cloudinary.com/dm9ig1vtk/image/upload/v1663018679/Dan%20Miller%20Gardens/image_vd6j6f.jpg",
+  },
+  {
+    text: "Lighting",
+    description: "",
+    img: "https://res.cloudinary.com/dm9ig1vtk/image/upload/v1663090583/Dan%20Miller%20Gardens/0a4f21ae06569fe2c8a2a7e11f39285a_pj2xy4.jpg",
+  },
+  {
+    text: "Irrigation",
+    description: "",
+    img: "https://res.cloudinary.com/dm9ig1vtk/image/upload/v1663089131/Dan%20Miller%20Gardens/eric-brehm-fZDIGIZ1e8E-unsplash_yfnmk7.jpg",
+  },
+];
+
+export default services
