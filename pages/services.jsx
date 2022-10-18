7647dd59afc5cad3d5027653bd643771c9d8066f
Craig McGowan <craigmcgowan15@gmail.com>
2022-10-17 22:09:31 -0400

    styles services page

--- pages/services.jsx
+++ pages/services.jsx
@@ -1,11 +1,12 @@
 import Carousel from "../components/Carousel.jsx"
+import styles from "../styles/Services.module.css"
 
 export default function Services() {
 
   
   return (
-    <>
+    <div className={styles.servicesPage}>
       <Carousel/>
-    </>
+    </div>
   )
 }
