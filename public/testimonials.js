1fd0df9990e270361cf8f7eb29cc731c8c9b5437
Craig McGowan <craigmcgowan15@gmail.com>
2022-10-23 14:45:48 -0400

    adds testimonials data

---
+++ public/testimonials.js
@@ -0,0 +1,29 @@
+const testimonials = [
+  {
+    name: "Donna V.",
+    review:
+      "Dan was great- best landscaper we’ve ever hired. He was quick to respond and accommodated a fast turnaround. He listened to my request for a colorful yard and had great suggestions. He was very professional and I highly recommend him.",
+  },
+  {
+    name: "Elise P.",
+    review:
+      "I’m so so glad I found Daniel! I wanted to put up lighting in my backyard but had no idea how to accomplish it. Daniel was prompt, professional, and gave thoughtful advice about options to consider and the pros and cons based on design and how it might feel in the space.",
+  },
+  {
+    name: "Kaitlin M.",
+    review:
+      "I showed him our drab outdoor area, explained my goals (some shade, mood lighting, and greenery), and he immediately had ideas for how to get the job done.",
+  },
+  {
+    name: "Barbara A.",
+    review:
+      "We’ve just started using him but we are delighted with what he’s done so far. He knows a lot about landscaping and other things so we are very grateful we found him.",
+  },
+  {
+    name: "Maia R.",
+    review:
+      "Dan was super professional when redoing our front driveway and garden area, along with our indoor planter and front door planters! They all look so amazing, and he was quick when working and diligent. ",
+  },
+];
+
+export default testimonials    
