ffedac2da01d71c7b52fef36d1c187030fd727d8
Craig McGowan <craigmcgowan15@gmail.com>
2023-03-02 21:57:30 -0500

    updates about page

--- pages/about.jsx
+++ pages/about.jsx
@@ -3,14 +3,14 @@ import styles from '../styles/About.module.css'
 export default function About() {
   return (
     <div className="page">
-      <h1>About Dan</h1>
+      <h1>About Dan Miller Gardens</h1>
       <img
         className={styles.headshot}
         src="https://res.cloudinary.com/dm9ig1vtk/image/upload/v1663032945/Dan%20Miller%20Gardens/DM_Headshot_myo4n6.webp"
         alt="Dan Miller"
       />
       <p className={styles.blurb}>
-        Dan MIller Gardens provides landscape design, installation and
+        Dan Miller Gardens provides landscape design, installation and
         maintenance. Our mission is to improve the appearance and functionality
         of your outdoor space. We aim to make our neighborhoods more beautiful
         and healthy places to live. Dan’s journey in horticulture started in
