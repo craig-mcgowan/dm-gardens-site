e2165f21f1eec11202b21bfa9122d9d18f9caaed
Craig McGowan <craigmcgowan15@gmail.com>
2023-03-17 15:38:48 -0400

    updates services.js

--- public/services.js
+++ public/services.js
@@ -1,10 +1,10 @@
 const services = [
   {
-    text: "Garden Design",
+    text: "Landscape Design and Installation",
     img: "https://res.cloudinary.com/dm9ig1vtk/image/upload/v1663018679/Dan%20Miller%20Gardens/image_3_ogitz3.jpg",
   },
   {
-    text: "Plantings",
+    text: "Hardscaping",
     description: "",
     img: "https://res.cloudinary.com/dm9ig1vtk/image/upload/v1663018679/Dan%20Miller%20Gardens/image_1_uo8tnt.jpg",
   },
@@ -14,14 +14,9 @@ const services = [
     img: "https://res.cloudinary.com/dm9ig1vtk/image/upload/v1663018679/Dan%20Miller%20Gardens/image_vd6j6f.jpg",
   },
   {
-    text: "Lighting",
+    text: "Mulching",
     description: "",
-    img: "https://res.cloudinary.com/dm9ig1vtk/image/upload/v1663090583/Dan%20Miller%20Gardens/0a4f21ae06569fe2c8a2a7e11f39285a_pj2xy4.jpg",
-  },
-  {
-    text: "Irrigation",
-    description: "",
-    img: "https://res.cloudinary.com/dm9ig1vtk/image/upload/v1663089131/Dan%20Miller%20Gardens/eric-brehm-fZDIGIZ1e8E-unsplash_yfnmk7.jpg",
+    img: "https://res.cloudinary.com/dm9ig1vtk/image/upload/v1663018679/Dan%20Miller%20Gardens/image_vd6j6f.jpg",
   },
 ];
 
