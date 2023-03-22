68fb02b612e89ef58fddf2967e80ed98a80a3fe6
Craig McGowan <craigmcgowan15@gmail.com>
2023-03-21 22:30:18 -0400

    mission.js updates

--- components/mission/mission.jsx
+++ components/mission/mission.jsx
@@ -1,11 +1,39 @@
 import styles from './Mission.module.css'
+import values from '../../public/values'
+import { GiSpade, GiHummingbird, GiSewingNeedle } from 'react-icons/gi'
+import {FaRecycle, FaHandshake, FaBusinessTime } from 'react-icons/fa'
+
+
 
 
 export default function Mission(props) {
+  // const iconsArr = [
+  //   { iconComp: GiHummingbird, name: "GiHummingBird" },
+  //   { iconComp: GiSpade, name: "GiSpade" },
+  //   { iconComp: FaHandshake, name: "FaHandshake" },
+  //   { iconComp: GiSewingNeedle, name: "GiSewingNeedle" },
+  //   { iconComp: FaBusinessTime, name: "FaBusinessTime" },
+  //   GiSewingNeedle,
+    
+  //   FaBusinessTime,
+  //   FaRecycle,
+  // ];
 
   return (
     <section>
-      MISSION SECTION
+      <div className={styles.sectionHeader}>Our Process</div>
+      {values.map(({ value, icon, blurb }) => {
+
+        
+        return (
+          <>
+            <p>{value}</p>
+            <Icon/>
+          </>
+        )
+      
+      })
+    }
     </section>
   )
 }
---
+++ public/values.js
@@ -0,0 +1,40 @@
+const values = [
+  {
+    value: "Custom Tailored Solutions",
+    blurb:
+      "Upon making an inquiry into our services, Dan will typically meet with a client to walk their property and discuss their specific needs. A unique estimate and proposal is then created for the customer to review. Revisions can be made as needed before the customer approves a final work order.",
+    icon: "GiSewingNeedle",
+  },
+  {
+    value: "Service-First Partnerships",
+    blurb:
+      "We are a service-first company, and our customers’ satisfaction is our top priority. We take great pride in creating and maintaining landscapes and we are always happy to answer any questions before, during or after the completion of a project.",
+    icon: "FaHandshake",
+  },
+  {
+    value: "Sustainable Landscaping & Gardening",
+    blurb:
+      "Minimizing environmental impact is a key part of our approach. Key concepts include minimizing landfill: We take into consideration the waste generated from our projects from start to finish. We use products with less packaging, and recycle and/or reuseleftover packaging when appropriate. When reworking an existing landscape,salvageable plants and materials are often donated. Waste that is compostable is composted. Our fleet of battery-powered equipment can be used in many situations, and theyreduce the emissions and noise that comes with gas engines. As the technology of battery-powered equipment improves, we will continue to make investments in tools that drive productivity and reduce our carbon footprint.",
+    icon: "FaRecycle",
+  },
+  {
+    value: "Environmental Stewardship",
+    blurb:
+      "We aim to create landscapes that blend with surrounding natural ecosystems. We believe it’s important to conserve local bird, bee and insect populations of a native landscape. Where applicable, we also planlandscapes to reduce stormwater runoff. We prefer plant material that is native and resistant to drought, which reduces watering needs.",
+    icon: "GiHummingbird",
+  },
+  {
+    value: "The Right Tools for the Job",
+    blurb:
+      "Using the best tool for the job is always a top priority in ourwork. Unfortunately landscaping equipment (particularly two-stroke gas engines) can often be noisy and dirty polluters. We keep our equipment in peak condition to ensure high quality work, reduce the risk of injury and keep them running as clean as possible.",
+    icon: "GiSpade",
+  },
+  {
+    value: "Affordable, Dependable, Quality Work",
+    blurb:
+      "We will work with you and your budget to give you the outdoor space you dream of. Dan's extensive knowledge and industry insight will help transform your landscaping. We know your time is valuable and always communicate clearly and work diligently to get your projects done.",
+    icon: "FaBusinessTime",
+  },
+];
+
+export default values
